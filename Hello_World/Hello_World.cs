85b43276158a6baa9fe5f0287bd9c7ec0139a470
ganeshkadole <kadoleg@gmail.com>
2022-06-29 20:57:37 +0530

    Added Hello_World

---
+++ Hello_World/Hello_World.cs
@@ -0,0 +1,23 @@
+﻿using NXOpen;
+public class Program
+{
+    private static Session theSession;
+    private static ListingWindow theLW;
+
+        public static void Main(string[] args)
+
+        {
+
+        theSession = Session.GetSession();
+        theLW = theSession.ListingWindow;
+        theLW.WriteLine("Hello World!");
+
+        }
+
+    public static int GetUnloadOption(string arg)
+    {
+        return System.Convert.ToInt32(Session.LibraryUnloadOption.Immediately);
+
+    }
+
+}
